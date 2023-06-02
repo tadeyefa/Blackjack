23b648d81758d43479130624f12140655d768e2a
tadeyefa <ohuntoluwase_adeyefa25@milton.edu>
2023-06-02 14:30:16 -0400

    Printed out all of the cards in the deck

--- src/Game.java
+++ src/Game.java
@@ -3,13 +3,15 @@ public class Game {
     public Card card2;
     public Card[] deck;
     public int count = 0;
+
     public static void main(String[] args) {
         Game blackjack = new Game();
     }
+
     public Game() {
         System.out.println("game constructor");
         deck = new Card[52];
-        for (int y=0;y<4;y++) {
+        for (int y = 0; y < 4; y++) {
             for (int x = 0; x < 13; x++) {
                 deck[count] = new Card(y, x);
                 count++;
