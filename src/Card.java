6bff846df0ea60a27b8394c67a17c331bed6c8dd
tadeyefa <ohuntoluwase_adeyefa25@milton.edu>
2023-06-02 13:09:23 -0400

    Printed out all of the cards in the deck

--- src/Card.java
+++ src/Card.java
@@ -6,7 +6,59 @@ public class Card {
 
     public Card(int pSuit, int pName) {
         if (pSuit == 0) {
-            suit = "Diamond";
+            suit = "Diamonds";
         }
+        if (pSuit == 1) {
+            suit = "Hearts";
+        }
+        if (pSuit == 2) {
+            suit = "Clubs";
+        }
+        if (pSuit == 3) {
+            suit = "Spades";
+        }
+        if (pName == 0) {
+            name = "Ace";
+        }
+        if (pName == 1) {
+            name = "Two";
+        }
+        if (pName == 2) {
+            name = "Three";
+        }
+        if (pName == 3) {
+            name = "Four";
+        }
+        if (pName == 4) {
+            name = "Five";
+        }
+        if (pName == 5) {
+            name = "Six";
+        }
+        if (pName == 6) {
+            name = "Seven";
+        }
+        if (pName == 7) {
+            name = "Eight";
+        }
+        if (pName == 8) {
+            name = "Nine";
+        }
+        if (pName == 9) {
+            name = "Ten";
+        }
+        if (pName == 10) {
+            name = "Jack";
+        }
+        if (pName == 11) {
+            name = "Queen";
+        }
+        if (pName == 12) {
+            name = "King";
+        }
+        System.out.println("You made the " + name + " of " + suit);
+    }
+    public void print() {
+        System.out.println("This card is the " + name + " of " + suit);
     }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,23 @@
 public class Game {
+    public Card card1;
+    public Card card2;
+    public Card[] deck;
+    public int count = 0;
     public static void main(String[] args) {
         Game blackjack = new Game();
     }
-
+    public Game() {
+        System.out.println("game constructor");
+        deck = new Card[52];
+        for (int y=0;y<4;y++) {
+            for (int x = 0; x < 13; x++) {
+                deck[count] = new Card(y, x);
+                count++;
+            }
+        }
+//        card1 = new Card(3, 5);
+//        card2 = new Card(2, 7);
+//        card1.print();
+//        card2.print();
+    }
 }
